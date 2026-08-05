a9775971caa0372d0f54da19352543ba5f5b3ad8
cnjphashi <webmaster@cnjphashi.com>
2026-08-05 11:48:23 +0800

    Update siteData.ts

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -224,61 +224,73 @@ export const facilities = [
     title: "DIY",
     description: "DIY Toys",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/DIY%E7%8E%A9%E5%85%B7.webp",
+    link: "https://cnjphashi.com/ja/product-category/omocha/diy/",
   },
   {
     title: "スポーツ玩具",
     description: "Sports Toys",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E8%BF%90%E5%8A%A8%E7%8E%A9%E5%85%B7.webp",
+    link: "https://cnjphashi.com/ja/product-category/omocha/supootsu/",
   },
   {
     title: "ぬいぐるみ",
     description: "Plush Toys",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E6%AF%9B%E7%BB%92%E7%8E%A9%E5%85%B7.webp",
+    link: "https://cnjphashi.com/ja/product-category/omocha/nuigurumi/",
   },
   {
     title: "パズル",
     description: " Puzzles",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E6%8B%BC%E5%9B%BE.webp",
+    link: "https://cnjphashi.com/ja/product-category/omocha/pazuru/",
   },
   {
     title: "プラスチック玩具",
     description: "Plastic Toys",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E5%A1%91%E6%96%99%E7%8E%A9%E5%85%B7.webp",
+    link: "https://cnjphashi.com/ja/product-category/omocha/purasuchikku-omocha/",
   },
   {
     title: "低単価玩具",
     description: "Low-cost Toys",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E4%BD%8E%E4%BB%B7%E7%8E%A9%E5%85%B7.webp",
+    link: "https://cnjphashi.com/ja/product-category/omocha/teitanka-omocha/",
   },
   {
     title: "光るおもちゃ",
     description: "Light-Up Toys",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E5%8F%91%E5%85%89%E7%8E%A9%E5%85%B7.webp",
+    link: "https://cnjphashi.com/ja/product-category/omocha/hikaru-omocha/",
   },
   {
     title: "木製おもちゃ",
     description: "Wooden Toys",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E6%9C%A8%E5%88%B6%E7%8E%A9%E5%85%B7.webp",
+    link: "https://cnjphashi.com/ja/product-category/omocha/mokusei-omocha/",
   },
   {
     title: "食玩",
     description: "Candy Toys",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E9%A3%9F%E7%8E%A9.webp",
+    link: "https://cnjphashi.com/ja/product-category/omocha/shokugan/",
   },
   {
     title: "シール",
     description: " Stickers",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E8%B4%B4%E7%BA%B8.webp",
+    link: "https://cnjphashi.com/ja/product-category/bunbougu/shiiru/",
   },
   {
     title: "バインダー",
     description: "Binders",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E6%B4%BB%E9%A1%B5%E5%A4%B9.webp",
+    link: "https://cnjphashi.com/ja/product-category/bunbougu/baindaa/",
   },
   {
     title: "ペン",
     description: "Pens",
     image: "https://cnjphashi.com/wp-content/uploads/2026/07/%E7%AC%94.webp",
+    link: "https://cnjphashi.com/ja/product-category/omocha/nuigurumi/",
   },
 ];
 
